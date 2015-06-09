27fab6c113dd65cacd77e14794307dc52a3fad0b
JornDallinga <jorndallinga@gmail.com>
2015-06-09 18:36:23 +0200

    Compiled the Kim et al. 2014 script to download forest cover change data and convert it to forest cover

--- R/Kim.R
+++ R/Kim.R
@@ -8,14 +8,26 @@ Kim <- function(Year){
   dir <- "data/" # If doesnt work add "./"
   
   # Download data
-  downloadPR(pr, Year, dir, log = NULL, baseURL = baseURL)
+  downloadPR(pr, Year, dir, log = NULL, baseURL = "ftp://ftp.glcf.umd.edu/glcf/")
   
+  # Create names for unpacking
+  p_filename <- sprintf("%03d", pr$PATH)
+  r_filename <- sprintf("%03d", pr$ROW)
+  pr_filename <- sprintf("p%sr%s_FCC_%s_CM.tif", p_filename, r_filename, Year)
+  
+  # create list of extraction map
+  x <- list.files(sprintf('%s/%s',dir,'extract_kim/'), full.names=FALSE)
+
   # Unpack VCF data
-  unpackVCF(pr=pr, year = Year, searchDir=dir, dir=sprintf('%s/%s',dir,'kim/'))
-  x <- list.files(sprintf('%s/%s',dir,'kim/'), full.names=TRUE)
+  if (pr_filename %in% x){
+    print("File allready exsist, no need for unpacking")
+  } else {
+    unpackVCF(pr=pr, year = Year, searchDir=dir, dir=sprintf('%s/%s',dir,'extract_kim/'))
+    
+  }
   
   # Load data into R environment
-  Raster <- raster(x[1])
+  Raster <- raster(sprintf("data/extract_kim/%s" , pr_filename))
   
   # Masking the Raster to the buffer area
   transform_buffer<- spTransform(buffer, CRS(proj4string(Raster))) 
@@ -25,12 +37,8 @@ Kim <- function(Year){
   Buffer_Crop <- crop(Masked_Raster, transform_buffer)
   
   # Set values and a value replacement function
-  Buffer_Crop[Buffer_Crop < Threshold] <- 0
-  Buffer_Crop[Buffer_Crop >= Threshold] <- 1
-  Buffer_Crop[Buffer_Crop > 100] <- NA
-  
-  # Clear directory to prevent extraction errors
-  unlink("data/extract_sexton/*.tif", recursive = FALSE)
+  Buffer_Crop[Buffer_Crop < 5 | Buffer_Crop > 20] <- 0
+  Buffer_Crop[Buffer_Crop > 10 & Buffer_Crop < 20] <- 1
   
   return (Buffer_Crop)
   

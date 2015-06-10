a778f7e0a140da8f04b5bd3871c0963942b08660
JornDallinga <jorndallinga@gmail.com>
2015-06-11 00:42:35 +0200

    removed redundant code lines

--- R/Hansen.R
+++ R/Hansen.R
@@ -15,15 +15,13 @@ Hansen <- function(Threshold){
   ## Calculate tiles needed to cover the AOI
   tiles <- calc_gfc_tiles(aoi)
   print(length(tiles))
-    
-  ## Crop the shapefile 
-  crop_buffer <- crop(tiles, aoi)
    
   ## Download GFC data
   download_tiles(tiles, 'data', first_and_last=FALSE)
-    
+  
   ## Extract data from tiles
-  gfc_extract <- extract_gfc(aoi, "data", filename="data/extract_hansen/GFC_extract.tif", overwrite=TRUE )
+  gfc_extract <- extract_gfc(aoi, "data", filename="data/extract_hansen/GFC_extract.tif", overwrite=TRUE)
+  
   
   ## Apply threshold to extracted data 
   gfc_thresholded <- threshold_gfc(gfc_extract, Threshold=Threshold, 
@@ -31,6 +29,7 @@ Hansen <- function(Threshold){
   
   ## Set mask over tresholded gfc, size of buffer
   mask_gfc <- mask(gfc_thresholded$forest2000, aoi)
+  
   
   return (mask_gfc)
 }

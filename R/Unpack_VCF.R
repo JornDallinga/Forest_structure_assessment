062b07144be86b7bc5a959e3bc62b3e0acc73ff8
JornDallinga <jorndallinga@gmail.com>
2015-06-25 02:15:31 +0200

    updated unpack function

--- R/Unpack_VCF.R
+++ R/Unpack_VCF.R
@@ -1,15 +1,24 @@
 # Unpack VCF data
 
 Unpack_VCF <- function(pr_filename, x, extract, Year, pr, dir){
-  if (pr_filename %in% x){
-    print("File allready exsist, checking if size == 0")
-    if (file.info(sprintf('%s/%s%s', dir, extract, pr_filename))$size == 0){
-      unlink(sprintf('%s/%s%s',dir, extract, pr_filename))
-      unpackVCF(pr=pr, year = Year, searchDir=dir, dir=sprintf('%s/%s',dir, extract))
+  t = 1
+  
+  for (i in x){
+    if (pr_filename[t] %in% x){
+      if (file.exists(sprintf('%s/%s%s', dir, extract, pr_filename[t]))){
+        print("File allready exsist, checking if file == 0 bytes")
+        if (file.info(sprintf('%s/%s%s', dir, extract, pr_filename[t]))$size == 0){
+          unlink(sprintf('%s/%s%s',dir, extract, pr_filename[t]))
+          unpackVCF(pr=pr, year = Year, searchDir=dir, dir=sprintf('%s/%s',dir, extract))
+        } else {
+        }
+      } else {
+        unpackVCF(pr=pr, year = Year, searchDir=dir, dir=sprintf('%s/%s',dir, extract))
+      }
+
     } else {
-      print("File allready exsist, no need for unpacking")
+      unpackVCF(pr=pr, year = Year, searchDir=dir, dir=sprintf('%s/%s',dir, extract))
     }
-  } else {
-    unpackVCF(pr=pr, year = Year, searchDir=dir, dir=sprintf('%s/%s',dir, extract))
+  t <- t + 1
   }
 }

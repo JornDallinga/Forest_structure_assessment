4b15e76437d1ddb5a489b4321d7be88c5c86c4ea
JornDallinga <jorndallinga@gmail.com>
2015-06-25 02:14:36 +0200

    Created mosaicing function

---
+++ R/Mosaic_Raster.R
@@ -0,0 +1,37 @@
+Mosaic_Raster <- function(x_list, dir, extract, buffer, pr_filename){
+  rast.list <- list()
+  for(i in 1:length(x_list)) {
+    list_raster <- sprintf("%s%s%s", dir, extract, x_list[i])
+    rast.list[i] <- raster(list_raster) 
+  }
+  
+  
+  if (length(rast.list) > 1){
+    t <- 1
+    new_list <- list()
+    for (i in rast.list){
+      
+      Raster <- stack(rast.list[t])
+      plot_Raster <- spTransform(buffer, CRS(proj4string(Raster))) 
+      plot_crop <- crop(Raster, plot_Raster)
+      reproject <- projectRaster(from = plot_crop, crs = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
+      new_list[t] <- reproject
+      
+      t <- t + 1
+    }
+    
+    new_list$fun <- max
+    new_list$tolerance <- 0.5
+    mosaic_dataset <- do.call(mosaic, new_list) 
+    mask_plot <- mask(mosaic_dataset, buffer)
+    
+  } else {
+    Raster <- raster(sprintf("%s%s%s", dir, extract, pr_filename))
+    plot_Raster <- spTransform(buffer, CRS(proj4string(Raster)))
+    plot_crop <- crop(Raster, plot_Raster)
+    reproject <- projectRaster(from = plot_crop, crs = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
+    mask_plot <- mask(reproject, buffer)
+  }
+    return (mask_plot)
+  
+}

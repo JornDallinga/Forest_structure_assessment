3fc50353c9aeac4992f5dc1f5fafe3df0a938afa
JornDallinga <jorndallinga@gmail.com>
2015-06-26 02:58:01 +0200

    created calculation of mean of forest cover for each chronosequence

---
+++ R/calc_mean.R
@@ -0,0 +1,23 @@
+calc_mean <- function(mat){
+  unique_test <- unique(mat$Chronosequence)
+  mean_matrix <- matrix(, nrow = length(unique_test), ncol = 3)
+  colnames(mean_matrix) <- c("Country", "Chronosequence", "Forest_cover")
+  t <- 1
+  
+  for (i in 1:length(unique_test)){
+    select_chrono <- subset(mat, grepl(unique_test[t], Chronosequence, fixed=TRUE))
+    
+    mean_matrix[t,1] <- unique(select_chrono$Country)
+    mean_matrix[t,2] <- unique(select_chrono$Chronosequence)
+    mean_matrix[t,3] <- mean(select_chrono$Forest_cover)
+    t <- t + 1
+  }
+  mat_year <- unique(mat$Year)
+  mat_buffer <- unique(mat$Buffer)
+  mat_threshold <- unique(mat$Threshold)
+  
+  test <- sprintf("Year%s_buffer%s_threshold%s", mat_year, mat_buffer, mat_threshold)
+  
+  mean_matrix -> test
+}
+

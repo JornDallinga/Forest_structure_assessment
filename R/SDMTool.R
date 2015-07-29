0e9869813162dfb88d2db84be4ef0e48796e017b
JornDallinga <jorndallinga@gmail.com>
2015-07-29 21:42:26 +0200

    solved error when the script would crash when an area had no forest cover at all.

--- R/SDMTool.R
+++ R/SDMTool.R
@@ -22,13 +22,15 @@ SDM_function <- function(S){
   ##### testing data frame
   
   
-  ####
+  #### Creating statistics from forest cover map
   c1.data <- ClassStat(S)
   
-  if (c1.data[1,1] == 0){
+  #### Set all values to 0 if the area has no forest cover
+  if (nrow(c1.data) == 1 & (c1.data)[1,1] == 0) {
+    c1.data <- c1.data[NA,]
+  } else if (nrow(c1.data) > 1 & (c1.data)[1,1] == 0) {
     c1.data <- c1.data[-1,]
-  } else {
-    
+  } else {   
   }
   
   ##### testing data frame

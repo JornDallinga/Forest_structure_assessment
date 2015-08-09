97d4bb48795c1d0d473f6b6554758d5fa3e054cd
JornDallinga <jorndallinga@gmail.com>
2015-08-10 00:10:32 +0200

    Writing outputs to excel and RDS files

---
+++ R/write_data.R
@@ -0,0 +1,12 @@
+Write_fun <- function(matrix_list){
+  dir.create(file.path('output/Excel'), showWarnings = FALSE)
+  dir.create(file.path('output/RDS'), showWarnings = FALSE)
+  j <- 1
+  for (i in matrix_list){
+    name_sheet <- names(matrix_list[j])
+    write.xlsx(matrix_list[j], file = sprintf("output/Excel/Buffer%s_Threshold%s_%s.xlsx", BufferDistance, Threshold, names(matrix_list[j])), sheetName = name_sheet)
+    saveRDS(matrix_list[j],file = sprintf("output/RDS/Buffer%s_Threshold%s_%s", BufferDistance, Threshold, name_sheet))
+    j <- j + 1
+  }
+  
+}

6253701cfc1c30d3f0a1d42a08c618438c5fa19f
JornDallinga <jorndallinga@gmail.com>
2015-08-13 13:33:50 +0200

    adding more variables to the mean calc functions

--- R/calc_mean.R
+++ R/calc_mean.R
@@ -1,7 +1,7 @@
 calc_mean <- function(mat){
   unique_test <- unique(mat$Chronosequence)
-  mean_matrix <- matrix(, nrow = length(unique_test), ncol = 3)
-  colnames(mean_matrix) <- c("Country", "Chronosequence", "Forest_cover")
+  mean_matrix <- matrix(, nrow = length(unique_test), ncol = 8)
+  colnames(mean_matrix) <- c("Country", "Chronosequence", "Forest_cover", "Water_cover", "Cloud_cover", "n_patches", "Total_edge", "Patch_cohesion")
   t <- 1
   
   for (i in 1:length(unique_test)){
@@ -10,16 +10,15 @@ calc_mean <- function(mat){
     mean_matrix[t,1] <- unique(select_chrono$Country)
     mean_matrix[t,2] <- unique(select_chrono$Chronosequence)
     mean_matrix[t,3] <- mean(select_chrono$Forest_cover)
+    mean_matrix[t,4] <- mean(select_chrono$Water_cover)
+    mean_matrix[t,5] <- mean(select_chrono$Cloud_cover)
+    mean_matrix[t,6] <- mean(select_chrono$n.patches)
+    mean_matrix[t,7] <- mean(select_chrono$total.edge)
+    mean_matrix[t,8] <- mean(select_chrono$patch.cohesion.index)
     t <- t + 1
   }
-  mat_year <- unique(mat$Year)
-  mat_buffer <- unique(mat$Buffer)
-  mat_threshold <- unique(mat$Threshold)
   
 
-  #sheetnam <- substitute(mat)
-  #sheetnam <- toString(sheetnam)
-  
   return (mean_matrix)
 }
 

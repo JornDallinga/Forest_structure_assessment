a98841991bd07346aad9b9e164d0e79c896b83a0
JornDallinga <jorndallinga@gmail.com>
2015-08-13 13:34:03 +0200

    Fixing bug in naming the columns of excel

--- R/write_data.R
+++ R/write_data.R
@@ -4,7 +4,7 @@ Write_fun <- function(matrix_list){
   j <- 1
   for (i in matrix_list){
     name_sheet <- names(matrix_list[j])
-    write.xlsx(matrix_list[j], file = sprintf("output/Excel/Buffer%s_Threshold%s_%s.xlsx", BufferDistance, Threshold, names(matrix_list[j])), sheetName = name_sheet)
+    write.xlsx(matrix_list[[j]], file = sprintf("output/Excel/Buffer%s_Threshold%s_Year%s.xlsx", BufferDistance, Threshold, Year), sheetName = names(matrix_list[j]), append = T)
     saveRDS(matrix_list[j],file = sprintf("output/RDS/Buffer%s_Threshold%s_%s", BufferDistance, Threshold, name_sheet))
     j <- j + 1
   }

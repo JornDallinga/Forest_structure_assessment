4113a17b1df65828c7ec78e06a7140a05e847714
JornDallinga <jorndallinga@gmail.com>
2015-08-21 20:32:22 +0200

    Fixed bug where in some data tiles the resolution was adjusted.
    Now a set resolution will be given to all data sets

--- R/Mosaic_Raster.R
+++ R/Mosaic_Raster.R
@@ -11,18 +11,19 @@ Mosaic_Raster <- function(x_list, dir, extract, buffer, pr_filename){
     new_list <- list()
     for (i in rast.list){
       
-      Raster <- stack(rast.list[t])
+      Raster <- rast.list[[t]]
       plot_Raster <- spTransform(buffer, CRS(proj4string(Raster))) 
       plot_crop <- crop(Raster, plot_Raster)
       reproject <- projectRaster(from = plot_crop, crs = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0", method = 'ngb')
+      res(reproject) <- c(0.00027,0.000271)
       new_list[t] <- reproject
-      
       t <- t + 1
     }
     
+    
     new_list$fun <- max
     new_list$tolerance <- 0.5
-    mosaic_dataset <- do.call(mosaic, new_list) 
+    mosaic_dataset <- do.call(mosaic, new_list)
     mask_plot <- mask(mosaic_dataset, buffer)
     
   } else {
@@ -35,3 +36,4 @@ Mosaic_Raster <- function(x_list, dir, extract, buffer, pr_filename){
     return (mask_plot)
   
 }
+

0a40e951982b363fb1a30b20f25aec5b4f88501e
JornDallinga <jorndallinga@gmail.com>
2015-09-05 00:35:34 +0200

    Adding functions

--- Main.R
+++ Main.R
@@ -16,6 +16,8 @@ if (!require(rgeos)) install.packages('rgeos')
 if (!require(sp)) install.packages('sp')
 if (!require(spatstat)) install.packages('spatstat')
 if (!require(devtools)) install.packages("devtools")
+if (!require(spatstat)) install.packages('car')
+if (!require(spatstat)) install.packages('fsmb')
 if (!require(MODIS)) install.packages("MODIS", repos="http://R-Forge.R-project.org")
 devtools::install_github('JornDallinga/VCF')
 
@@ -31,6 +33,8 @@ library (devtools)
 library (VCF)
 library (plyr)
 library (xlsx)
+library (car)
+
 
 
 ###------------------------------------- Source Scripts ----------------------------------
@@ -51,6 +55,9 @@ source("R/Mosaic_Raster.R")
 source("R/Plotting.R")
 source("R/calc_mean.R")
 source("R/plot_Figures.R")
+source("R/Write_chrono_excel.R")
+source("R/vif_func.R")
+
 
 
 
@@ -68,8 +75,8 @@ dir.create(file.path('data/extract_hansen'), showWarnings = FALSE)
 ### Set variables by user
 #Countrycode <- "CRI"      # See: http://en.wikipedia.org/wiki/ISO_3166-1
 #Chronosequence <- NULL    # Chronosequence within the country
-Year <- 2000              # Only applies to Sexton script
-BufferDistance <- 10000    # Distance in meters
+Year <- 2000             # Only applies to Sexton script
+BufferDistance <- 1000   # Distance in meters
 Threshold <- 30           # Cells with values greater than threshold are classified as 'Forest'
 
 
@@ -78,12 +85,12 @@ setInternet2(use = TRUE)
 ###------------------------------------- Create Matrix for results ----------------------
 
 ## reading excel file
-mydata <- read.xlsx("Chrono_Coords_list_R_Ready.xlsx", 3)
+mydata <- read.xlsx("Chrono_Coords_list_R_Ready.xlsx", 4)
 countcoords <- nrow(mydata)
 
 
 #creating empty matrix
-mat <- matrix(, nrow = countcoords, ncol = 48)
+mat <- matrix(, nrow = countcoords, ncol = 49)
 
 #creating empty dataframe
 mat <- data.frame(mat)
@@ -105,13 +112,14 @@ for(i in 1:countcoords) {
   pb <- winProgressBar(title = "progress bar", min = 0,
                        max = countcoords, width = 300)
   Sys.sleep(0.1)
-  setWinProgressBar(pb, i, title=paste( round(i/countcoords*100, 0),
+  setWinProgressBar(pb, i, title=paste(round(i/countcoords*100, 0),
                                         "% done"))
   
   
   ## reading country code and chronosequence from mydata
   Countrycode <- levels(mydata$Country[1 + j])[mydata$Country[1 + j]]
   Chronosequence <- levels(mydata$Chronosequence[1 + j])[mydata$Chronosequence[1 + j]]
+  Plot_ID <- as.character(mydata$Plot_ID[1 + j])
 
   ###------------------------------------ Run functions -----------------------------------
 
@@ -119,7 +127,7 @@ for(i in 1:countcoords) {
   Buffer_Point(Countrycode, BufferDistance) #Places a .rds file in the data folder
   
   ## Analysis Forest data
-  matrix_list <- Forest_Analysis(Year = Year, Countrycode = Countrycode, Chronosequence = Chronosequence, BufferDistance = BufferDistance, Threshold = Threshold)
+  matrix_list <- Forest_Analysis(Year = Year, Countrycode = Countrycode, Chronosequence = Chronosequence, Plot_ID = Plot_ID, BufferDistance = BufferDistance, Threshold = Threshold)
   
   # Clear directory to prevent extraction errors
   unlink("data/BufferWGS.rds", recursive = FALSE)
@@ -132,6 +140,10 @@ for(i in 1:countcoords) {
     Write_fun(matrix_list)
     mat_list <- lapply(matrix_list, calc_mean)
     lapply(1:length(mat_list), function(i) write.xlsx(mat_list[[i]], file = sprintf("output/Excel/mean_Buffer%s_Threshold%s_Year%s.xlsx", BufferDistance, Threshold, Year), sheetName = names(mat_list[i]), append = T))
+    
+    ## Writing individual chronosequences excel files to folder
+    Write_chrono_excel(matrix_list)
+    
     print("Done")
     close(pb)
   } else {
@@ -139,32 +151,3 @@ for(i in 1:countcoords) {
   }
   
 }
-
-
-
-
-###------------------------------------- Testing area -----------------------------------
-
-# Assign plot fragmentation function to variables
-plot_Sexton <- SDM_plot(S)
-plot_Hansen <- SDM_plot(H)
-
-png(filename="output/Fragmentation.png")
-par(mfrow=c(2,2))
-
-# Plot forest/non-forest
-plot(New_S, main = "Sexton")
-plot(H, main = "Hansen")
-
-# Plot the fragmentation of Sexton and Hansen
-plot(New_proj_Sexton, main = "Sexton")
-plot(plot_Hansen, main = "Hansen")
-dev.off()
-
-###------------------------------------ Summerize/Visualize output -----------------------
-
-## Compare output Sexton and Hansen data
-## Visualize in scatterplot
-png(filename="output/Scatterplot.png")
-plot(mat[,2], mat[,3], main = "Difference in nr. of patches between Sexton and Hansen data", xlab="Sexton", ylab="Hansen", col = "red" )
-dev.off()

a5fee220ed440a7927413befc31105937f557863
JornDallinga <jorndallinga@gmail.com>
2015-09-09 19:51:11 +0200

    new calc mean function to select all variables

--- R/calc_mean.R
+++ R/calc_mean.R
@@ -1,24 +1,34 @@
 calc_mean <- function(mat){
   unique_test <- unique(mat$Chronosequence)
-  mean_matrix <- matrix(, nrow = length(unique_test), ncol = 8)
-  colnames(mean_matrix) <- c("Country", "Chronosequence", "Forest_cover", "Water_cover", "Cloud_cover", "n_patches", "Total_edge", "Patch_cohesion")
+  mean_matrix <- matrix(, nrow = length(unique_test), ncol = 43)
+  #norm_matrix <- matrix(, nrow = length(unique_test), ncol = 43)
+  mean_matrix <- data.frame(mean_matrix)
+  colnames(mean_matrix)[1:2] <- names(mat)[1:2]
+  suppressWarnings(colnames(mean_matrix)[3:43] <- names(mat)[9:50])
+
   t <- 1
   
   for (i in 1:length(unique_test)){
     select_chrono <- subset(mat, grepl(unique_test[t], Chronosequence, fixed=TRUE))
     
-    mean_matrix[t,1] <- unique(select_chrono$Country)
-    mean_matrix[t,2] <- unique(select_chrono$Chronosequence)
-    mean_matrix[t,3] <- mean(select_chrono$Forest_cover)
-    mean_matrix[t,4] <- mean(select_chrono$Water_cover)
-    mean_matrix[t,5] <- mean(select_chrono$Cloud_cover)
-    mean_matrix[t,6] <- mean(select_chrono$n.patches)
-    mean_matrix[t,7] <- mean(select_chrono$total.edge)
-    mean_matrix[t,8] <- mean(select_chrono$patch.cohesion.index)
+    #scaled.dat <- select_chrono
+    #scaled.dat[9:49] <- scale(select_chrono[9:49], center = T, scale = apply(select_chrono[9:49], 2, sd, na.rm = TRUE))
+    #scaled.dat[9:49] <- scale(select_chrono[9:49])
+    
+    #colMeans(scaled.dat[9:49])  # faster version of apply(scaled.dat, 2, mean)
+    #scaled.dat[9:49] <- apply(scaled.dat[9:49], 2, sd)
+    
+    mean_matrix[t,1:2] <- c(unique(select_chrono$Country), unique(select_chrono$Chronosequence))
+    
+    mean_matrix[t,1:2] <- c(unique(select_chrono$Country), unique(select_chrono$Chronosequence))
+    mean_matrix[t,3:length(mean_matrix)]<- colMeans(select_chrono[9:length(select_chrono)])
+    
+    
+
     t <- t + 1
   }
-  
 
+  
+  
   return (mean_matrix)
 }
-
